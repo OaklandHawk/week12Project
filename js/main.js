ff5003679b5d9f08ce0f590f3631eead5f199307
OaklandHawk <hawks93187@gmail.com>
2018-08-16 13:40:27 -0700

    new commit

--- js/main.js
+++ js/main.js
@@ -1,50 +1,48 @@
-
-    var user = {
-        name: "Evan Winston",
-        image: "img/evan.jpg",
-        progress: 0.66
-    }
-
-    var courseArr = [{
-            title: "Readings in World Literature",
-            subject: "Social Studies"
-        }, {
-            title: "Theory of Numbers",
-            subject: "Mathematics"
-        }, {
-            title: "Farsi 301",
-            subject: "Languages"
-        }];
-
-    var activityArr = [{
-            title: "Completed SOSC 503 Module 7 Quiz",
-            subject: "Submitted for review 6/11/18 at 10:39PM"
-        }, {
-            title: "Commented in MATH104 Tuesday Discussion Group",
-            subject: "Lorem ipsum dolores sit..."
-        }, {
-            title: "Enrolled in FARS301",
-            subject: "Cohort begins 9/08/18"
-        }];
-
-    var accountArr = [{
-            title: "Enrolled since: ",
-            subject: "9/06/2017"
-        }, {
-            title: "Enrollment Status: ",
-            subject: "Active Student"
-        }, {
-            title: "Department",
-            subject: "Computer Science"
-        }];
-
-function setUser(user, image, name, id) {
-    img.src = user.image;
-    img.alt = user.name;
-    name.innerText = user.name;
-    id.innerText = 'User #:' + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10)
+var user = {
+    name: "Taylor Lyles",
+    image: "img/Taylor.jpg",
+    progress: 0.66
 }
 
+var courseArr = [{
+        title: "Readings in World Literature",
+        subject: "Social Studies"
+    }, {
+        title: "Theory of Numbers",
+        subject: "Mathematics"
+    }, {
+        title: "Farsi 301",
+        subject: "Languages"
+    }];
+
+var activityArr = [{
+        title: "Completed SOSC 503 Module 7 Quiz",
+        subject: "Submitted for review 6/11/18 at 10:39PM"
+    }, {
+        title: "Commented in MATH104 Tuesday Discussion Group",
+        subject: "Lorem ipsum dolores sit..."
+    }, {
+        title: "Enrolled in FARS301",
+        subject: "Cohort begins 9/08/18"
+    }];
+
+var accountArr = [{
+        title: "Enrolled since: ",
+        subject: "9/06/2017"
+    }, {
+        title: "Enrollment Status: ",
+        subject: "Active Student"
+    }, {
+        title: "Department",
+        subject: "Computer Science"
+    }];
+
+function setUser(user, image, name, id) {
+    image.src = user.image;
+    image.alt = user.name;
+    name.innerText = user.name;
+    id.innerText = "User #: " + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10); 
+}
 setUser(user, document.getElementById("userImg"), document.getElementById("username"), document.getElementById("userId"));
 
 function formatTime(time) {
@@ -75,7 +73,6 @@ function setTime(el) {
     }, 1000)
 }
 setTime(document.getElementById("time"));
-
 
 function setActive(arr, title, list, str) {
     title.innerText = str
